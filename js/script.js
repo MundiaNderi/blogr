6f84df1417529c1df21037665abbc89f2ba9f10d
Serah Nderi <sarahngima77@gmail.com>
2023-12-15 14:03:20 +0300

    sub menus;

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-function toggleSubMenu(event) {
-    event.preventDefault();
+// Target the product submenu
 
-    const subMenu = document.getElementById("sub-menu");
-    subMenu.classList.toggle("hidden")
-}
+const productLink = document.getElementById("product-link");
+const subMenu = document.getElementById("sub-menu");
+
+productLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  subMenu.classList.toggle("hidden");
+});
+
+
+// Target the connect submenu
+
+const connectLink = document.getElementById("connect-link");
+const subMenu1 = document.getElementById('sub-menu1');
+
+connectLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    subMenu1.classList.toggle("hidden");
+} )
+
+// Connect the company sub-menu
+
+const companyLink = document.getElementById("company-link");
+const subMenu2 = document.getElementById("sub-menu2");
+
+companyLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    subMenu2.classList.toggle("hidden")
+})
